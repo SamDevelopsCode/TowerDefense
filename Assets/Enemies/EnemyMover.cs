c6cd06006acc6efb349bcb0cc9bf82166288073b
_booch <fousek.sam@gmail.com>
2023-12-15 23:03:05 -0600

    added smooth movement to enemy

--- Assets/Enemies/EnemyMover.cs
+++ Assets/Enemies/EnemyMover.cs
@@ -6,9 +6,10 @@ using UnityEngine.Serialization;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] private float _waitTimeUntilMoveAgain = 1.5f; 
+    
+    [SerializeField][Range(0f, 5f)] private float _moveSpeed = 2.0f;
     [SerializeField] private List<GridPoint> path = new List<GridPoint>();
-
+    
     private void Start()
     {
         StartCoroutine(MoveAlongPath());
@@ -18,12 +19,19 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (var gridPoint in path)
         {
-            var nextPosition = new Vector3(gridPoint.transform.position.x, 0, gridPoint.transform.position.z);
-            print(gridPoint.name);
-            print(nextPosition);
-            transform.position = nextPosition;
-            print("player's pos: " + transform.position);
-            yield return new WaitForSeconds(_waitTimeUntilMoveAgain);
+            var startPosition = transform.position;
+            var endPosition = gridPoint.transform.position;
+            var travelPercent = 0f;
+            
+            //while we haven't arrived at the next grid's position
+            while (travelPercent < 1f)
+            {
+                //add a small bit 
+                travelPercent += Time.deltaTime * _moveSpeed;
+                transform.LookAt(gridPoint.transform);
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
     
